47bb2ef28a53b0649ca6ad2f5b2ce0251e6f7d15
ポルトラ <nakajinn.1up@gmail.com>
2025-07-13 16:02:36 +0000

    何食べ

--- src/App.tsx
+++ src/App.tsx
@@ -8,8 +8,12 @@ import './App.css';
 // fanction App(){この部分}　この部分がアプリの構造部分　コンポーネント */
 export default function App() {
 
-// menuとsetmenuを定義しておく
-  const [menu, setMenu] = useState('');
+// mとsを定義しておく
+  const [meat, setMeat] = useState('');
+  const [vege, setVege] = useState('');
+  const [method, setMethod] = useState('');
+  const [result, setResult] = useState('');
+
 //** ランダム表示のための機能
 // Tにすると数字(number)でも文字(string)でも使えるらしい
 // arr以降で　1つ取り出す　という動作を行なっているが
@@ -22,25 +26,57 @@ export default function App() {
 // const で定義したものをそのまま埋め込めるので、これを使わないと
 // return pickRandom [鶏肉,豚肉,~]となってしまうので、使っている
 //   */
-  function makeRandomMenu(): string{
-    return `${pickRandom(niku)} ✖️ ${pickRandom(yasai)} ✖️ ${pickRandom(houhou)}`;
-  }
 
+  const handleRandomMeat = () => setMeat(pickRandom(niku));
+  const handleRandomVege = () => setVege(pickRandom(yasai));
+  const handleRandomMethod = () => setMethod(pickRandom(houhou));
+
+  const handleResult = () => {
+    if (meat && vege && method) {
+      setResult(`${meat} ✖️ ${vege} ✖️ ${method}`);
+    } else {
+      setResult('全てランダムにしてから結果を表示してください');
+    } 
+    };
+    const handleReset = () => {
+      setMeat(pickRandom(niku));
+      setVege(pickRandom(yasai));
+      setMethod(pickRandom(houhou));
+      setResult('');
+    };
 
     return (
     <div className='container'>
       <h1 className='title'>何食べる？</h1>
       {/* ランダムメニュー表示ボタン */}
       <div className='content'>
-      <button
-      onClick={() => setMenu(makeRandomMenu())}
-      className='button'
-      >
-        ランダムメニューを表示
-      </button>
-      {/*メニュー表示 */}
-      <p className='menu'>{menu}</p>
-      </div>    
+      <div className='random-bottuons'>
+      <button onClick={handleRandomMeat}>肉をランダム</button>
+      <button onClick={handleRandomVege}>野菜をランダム</button>
+      <button onClick={handleRandomMethod}>調理法をランダム</button>
+      </div>
+    
+    <div className='display-items'>
+      <div className='item'>
+        <p>{meat}</p>
+      <button onClick={handleRandomMeat}>肉を引き直す</button>
+      </div>
+      <div className='item'>
+        <p>{vege}</p>
+      <button onClick={handleRandomVege}>野菜を引き直す</button>
+      </div>
+      <div className='item'>
+        <p>{method}</p>
+      <button onClick={handleRandomMethod}>調理法を引き直す</button>
+      </div>
     </div>
+    <div className='result-buttons'>
+      <button onClick={handleResult}>結果</button>
+      <button onClick={handleReset}>引き直す</button>
+    </div>
+      <p className='menu'>{result}</p>    
+    </div>
+  </div>
   );
 }
+//チャットGPTに画像を送れる制限が来たため終了
